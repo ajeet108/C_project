e47f04df493b39020b6a626801f1a201d53bc222
ajeet108 <160268820+ajeet108@users.noreply.github.com>
2024-02-22 11:18:51 +0530

    Update longestOne.c

--- longestOne.c
+++ longestOne.c
@@ -2,10 +2,12 @@
 #include<stdlib.h>
 int main()
 {
-	int data,longest=0,bit=31,count=0,count1=0;
-	int res;
+	int data,longest=0,bit=31,count=0,i;
 	puts("enter data:");
 	scanf("%d",&data);
+	for(i=31;i>=0;i--)
+		printf("%d",((data>>i)&1));
+	printf("\n");
 	while(bit>=0)
 	{
 		if(data&(1<<bit))
