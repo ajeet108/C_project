a6afb41a4342edf60a7696df7b35a3a8c3338a07
ajeet108 <160268820+ajeet108@users.noreply.github.com>
2024-02-22 12:00:43 +0530

    Add files via upload

---
+++ inputbuffer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("hello");
+	//while(1);// both are same
+	//it will not print hello or any thing because we did not clear input buffer
+	//for getting output we need to write \n ya fflush or fflashall ...
+	while(1)
+	{
+
+	}
+}
