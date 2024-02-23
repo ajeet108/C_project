f517b1d3394b83dcba84ccc19322e9a3b096e7f3
ajeet108 <160268820+ajeet108@users.noreply.github.com>
2024-02-23 12:43:08 +0530

    Add files via upload

---
+++ starinCenter1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,j,n;
+	printf("enter num\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++,printf("\n"))
+	{
+		for(j=n;j>=1;j--)
+		{
+			(j>i)?printf(" "):printf("* ");
+		}
+	}
+	for(i=1;i<=n;i++,printf("\n"))
+	{
+		for(j=1;j<=n;j++)
+		{
+			(j<=i)?printf(" "):printf("* ");
+		}
+	}
+}
